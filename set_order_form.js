02f1d61f6d4377d1dcb619d829c8a8aa02c0f691
ninotoshi <toshi@Toshis-MacBook-Pro.local>
2014-07-11 16:03:52 +0900

    created JS code

---
+++ set_order_form.js
@@ -0,0 +1,11 @@
+var t=((window.getSelection&&window.getSelection())||(document.getSelection&&document.getSelection())||(document.selection&&document.selection.createRange&&document.selection.createRange().text)).toString()||document.body.innerText;
+var f=document.createElement("form");
+f.action="https://gengo.com/order/receive_job_post/";
+f.method="post";
+var i=document.createElement("input");
+i.type="text";
+i.name="body_src";
+i.value=t;
+f.appendChild(i);
+document.body.appendChild(f);
+f.submit();
